4faad61cf935f71f548c2325d3fe06b5b7cbd70b
Юрий Новиков <iurij.novickov2016@yandex.ru>
2022-01-27 21:42:10 +0300

    111

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'image', 'group', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description')
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'post', 'text', 'created')
+    search_fields = ('text',)
+    list_filter = ('created',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -3,20 +3,46 @@ from django.db import models
 
 User = get_user_model()
 
-
 class Post(models.Model):
-    text = models.TextField()
+    """Модель отдельной публикации"""
+    text = models.TextField('Текст публикации',
+                            help_text='Введите текст сообщения')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        verbose_name='Автор публикации',
+        on_delete=models.CASCADE,
+        related_name='posts',
+        help_text='Выберите автора публикации'
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        verbose_name='Картинка для поста',
+        upload_to='posts/',
+        null=True,
+        blank=True,
+        help_text='Картинка для поста'
+    )
+    group = models.ForeignKey(
+        'Group',
+        verbose_name='Группа',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='group_label',
+        help_text='Выберите группу'
+    )
 
-    def __str__(self):
-        return self.text
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self) -> str:
+        return self.text[:15]
 
 
 class Comment(models.Model):
+    """Модель комментария к публикации"""
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
@@ -24,3 +50,12 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+class Group(models.Model):
+    """Модель сообщества"""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
